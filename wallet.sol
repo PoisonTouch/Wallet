0e2a07cd614fb6a429c31fcfde9e8b72d997241b
PoisonTouch <82893327+PoisonTouch@users.noreply.github.com>
2021-10-24 23:55:18 +0300

    a

--- wallet.sol
+++ wallet.sol
@@ -16,23 +16,56 @@ contract wallet {
     // Contract can have a `constructor` – function that will be called when contract will be deployed to the blockchain.
     // In this example constructor adds current time to the instance variable.
     // All contracts need call tvm.accept(); for succeeded deploy
+    /// @dev Contract constructor.
     constructor() public {
-        // Check that contract's public key is set
+        // check that contract's public key is set
         require(tvm.pubkey() != 0, 101);
-        // Check that message has signature (msg.pubkey() is not zero) and
-        // message is signed with the owner's private key
+        // Check that message has signature (msg.pubkey() is not zero) and message is signed with the owner's private key
         require(msg.pubkey() == tvm.pubkey(), 102);
-        // The current smart contract agrees to buy some gas to finish the
-        // current transaction. This actions required to process external
-        // messages, which bring no value (henceno gas) with themselves.
         tvm.accept();
+    }
 
-        timestamp = now;
+
+    // Modifier that allows function to accept external call only if it was signed
+    // with contract owner's public key.
+    modifier checkOwnerAndAccept {
+        // Check that inbound message was signed with owner's public key.
+        // Runtime function that obtains sender's public key.
+        require(msg.pubkey() == tvm.pubkey(), 100);
+
+		// Runtime function that allows contract to process inbound messages spending
+		// its own resources (it's necessary if contract should process all inbound messages,
+		// not only those that carry value with them).
+		tvm.accept();
+		_;
+	}
+
+    /// @dev Allows to transfer tons to the destination account.
+    /// @param dest Transfer target address.
+    /// @param value Nanotons value to transfer.
+    /// @param bounce Flag that enables bounce message in case of target contract error.
+    function sendTransaction(address dest, uint128 value, bool bounce,uint16 flag) public pure checkOwnerAndAccept {
+         // Runtime function that allows to make a transfer with arbitrary settings.
+        dest.transfer(value, bounce, flag);
+    }
+
+    //функция отправки средств без комиссии за свой счёт
+    function sendWithoutCom(address dest,uint128 value) public pure checkOwnerAndAccept 
+    {
+        sendTransaction(dest,value,false,0);
+    }
+
+    //функция отправки средств без комиссии за свой счёт
+    function sendWithCom(address dest,uint128 value) public pure checkOwnerAndAccept 
+    {
+        sendTransaction(dest,value,false,1);
+    }
+
+    //функция отправки всех средств и уничтожения кошелька
+    function sendAllAndDestroy(address dest) public pure checkOwnerAndAccept 
+    {
+        sendTransaction(dest,1,false,160);
     }
 
     
-
-    
-
-   
 }
